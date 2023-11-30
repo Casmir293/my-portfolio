5c27b272a03ce21bd8a1df9ec6fd75c90d5b51e4
Casmir Onyeka <casmir293@gmail.com>
2023-11-30 08:53:05 +0100

    added onload to JS isotope

--- js/main.js
+++ js/main.js
@@ -79,14 +79,16 @@
   );
 
   // Portfolio filter
-  var portfolioIsotope = $(".portfolio-container").isotope({
-    itemSelector: ".portfolio-item",
-    layoutMode: "fitRows",
-  });
+  $(window).on("load", function () {
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
+    });
 
-  $("#portfolio-filter li").on("click", function () {
-    $("#portfolio-filter li").removeClass("filter-active");
-    $(this).addClass("filter-active");
-    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+    $("#portfolio-filter li").on("click", function () {
+      $("#portfolio-filter li").removeClass("filter-active");
+      $(this).addClass("filter-active");
+      portfolioIsotope.isotope({ filter: $(this).data("filter") });
+    });
   });
 })(jQuery);
